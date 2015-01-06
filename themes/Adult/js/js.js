808c5edad4bc77e663f368cc737e860696d1a2ef
Davis Gao <naitnix@126.com>
2015-01-06 12:54:54 +0800

    m

--- themes/Adult/js/js.js
+++
@@ -1,6 +0,0 @@
-// JavaScript Document
-function divheight(id){
-    var divh = document.getElementById(id);
-    var number1 = divh.scrollHeight;
-    if( (number1 % 2) !== 0){divh.style.height = number1+1+'px'}
-}
